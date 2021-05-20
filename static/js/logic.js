ce14449636feb1f6758905db8de52310e58fa8ad
Molly Sullivan <mchandler02@gmail.com>
2021-05-19 21:42:56 -0500

    css updated, map displaying

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,13 +1,29 @@
-var myMap = L.map("map", {
-    center: [0, 0], //check center
-    zoom: 2 //check zoom
-})
+console.log("logic.js importing")
 
+
+// L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+//     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+//     tileSize: 512,
+//     maxZoom: 18,
+//     zoomOffset: -1,
+//     id: 'mapbox/streets-v11',
+//     accessToken: API_KEY
+// }).addTo(myMap);
+
+// var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
+
+var myMap = L.map("mapid", {
+    center: [0, 0], //latitude comes first
+    zoom: 2
+});
+// Adding a tile layer (the background map image) to our map
+// We use the addTo method to add objects to our map
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
     maxZoom: 18,
     zoomOffset: -1,
-    id: satellite-v9,
-    accessToken = API_KEY
-}).addTo(myMap);
+    id: "mapbox/streets-v11", //many different types of maps available
+    accessToken: API_KEY
+}).addTo(myMap); //this is what adds the tile layer to the map
+console.log(myMap)
